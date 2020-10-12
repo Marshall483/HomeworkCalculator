3e85db4f138076589049fa3a5843189a43c3c36b
Marshall483 <jows.17@mail.ru>
2020-10-12 17:40:48 +0300

    Added middleware

---
+++ middleware/Client F#/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+    
+//Create request
+
+//Get responce
+
+//Parse response
+
+//Show response
+
+[<EntryPoint>]
+let main argv =
+
+    let first = Int32.Parse(Console.ReadLine())
+    let action = Console.ReadLine()
+    let second = Int32.Parse(Console.ReadLine())
+
+    0
---
+++ middleware/Server-Middleware/CalculatorMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace CalcOnline.Middleware
+{
+    public class CalculatorMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public CalculatorMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string first = context.Request.Query["first"];
+            string second = context.Request.Query["second"];
+            string action = context.Request.Query["action"];
+
+            if (first == null || second == null || action == null)
+                return;
+
+            var res = Calculator.Calculate(int.Parse(first), int.Parse(second), action);
+
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class CalculatorExtention
+    {
+        public static IApplicationBuilder UseCalculator(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CalculatorMiddleware>();
+        }
+    }
+}
