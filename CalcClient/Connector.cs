8f8ae725ba49d36ab48d74f10f1c20a9e9ed1761
Marshall483 <jows.17@mail.ru>
2020-10-26 20:18:42 +0300

    Added C# client

---
+++ CalcClient/Connector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingApp
+{
+    class Connector
+    {
+        private string _url;
+
+        public Connector(string url) => _url = url;
+
+        private string FormateAction(string action) => 
+                action.
+                Replace("+", "%2B").
+                Replace("*", "%2A").
+                Replace("/", "%2F");
+
+
+        private string CreateUrl(double first, double second, string action)
+        {
+            string formatedAct = FormateAction(action);
+            string url = _url + string.Format("first={0}&second={1}&action={2}",
+                first, second, formatedAct);
+            return url;
+        }
+
+        private WebRequest CreateRequest(double first, double second, string action)
+        {
+           return  WebRequest.Create(CreateUrl(first, second, action));
+        }
+
+        private async Task<WebResponse> GetResponseAsync(WebRequest _request)
+        {
+            if (_request != null)
+                return await _request.GetResponseAsync();
+            else return null;
+        }
+
+        private string GetAnswer(WebResponse response)
+        {
+            switch (response.Headers["result"])
+            {
+                case "Fail":
+                    return string.Empty;
+                default:
+                    return response.Headers["result"];
+            }
+        }
+
+        public async Task<string> Solve(double first, string action, double second)
+        {
+            WebRequest request = CreateRequest(first, second, action);
+            WebResponse responce = await GetResponseAsync(request);
+            string result = GetAnswer(responce);
+            return await Task.FromResult(result);
+
+        }
+    }
+}
---
+++ CalcClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace TrainingApp
+{
+    class Program
+    {
+
+        static void ExpressionAddition(double first, double second)
+        {
+            MethodInfo consoleWr = typeof(Console).GetMethod( "WriteLine" , new[] { typeof(double) });
+            ParameterExpression _first = Expression.Parameter(typeof(double), "first");
+            ParameterExpression _second = Expression.Parameter(typeof(double), "second");
+
+            Expression[] param = new[] { _first, _second };
+
+            BinaryExpression add = BinaryExpression.Add(_first, _second);
+            MethodCallExpression call = Expression.Call(null, consoleWr, add);
+
+            Expression<Action<double, double>> lambda = Expression.Lambda<Action<double, double>>(call, new[] { _first, _second });
+
+            Action<double, double> compiled = lambda.Compile();
+
+            compiled(first, second);
+
+        }
+        static void Main(string[] args)
+        {
+            ExpressionAddition(10, 20);
+            Console.ReadKey();
+        }
+    }
+}
