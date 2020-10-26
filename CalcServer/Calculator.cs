33b6621e0d77ec1ef5710d900c77206cee8dc32c
Marshall483 <jows.17@mail.ru>
2020-10-26 19:49:20 +0300

    update Server

---
+++ CalcServer/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalcOnline.Middleware
+{
+    public class Calculator
+    {
+
+        static int Sum(int first, int second) => first + second;
+        static int Difference(int first, int second) => first - second;
+        public static int Divide(int first, int second)
+        {
+            if (second != 0) return first / second;
+            else return int.MinValue;
+        }
+        static int Mult(int first, int second) => first * second;
+
+        public static int Calculate(int first, int second, string action)
+        {
+            switch (action)
+            {
+                case "+": return Sum(first, second);
+                case "-": return Difference(first, second);
+                case "*": return Mult(first, second);
+                case "/": return Divide(first, second);
+                default: return int.MinValue;
+            }
+        }
+    }
+}
---
+++ CalcServer/CalculatorMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace CalcOnline.Middleware
+{
+    public class CalculatorMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public CalculatorMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public void WriteResultAtHeaders(HttpContext context, string result) {
+            if (result != "Fail") context.Response.Headers.Add("result", result);
+            else context.Response.Headers.Add("result", result.ToString()); }
+
+ 
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Query.Count == 0)
+                return;
+
+            string first = context.Request.Query["first"];
+            string second = context.Request.Query["second"];
+            string action = context.Request.Query["action"];
+
+
+            var res = Calculator.Calculate(int.Parse(first), int.Parse(second), action);
+
+            if (res != int.MinValue)
+            {
+                WriteResultAtHeaders(context, res.ToString());
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+            }
+            else
+                WriteResultAtHeaders(context, "Fail");
+
+            await _next.Invoke(context);
+        }
+    }
+
+    public static class CalculatorExtention
+    {
+        public static IApplicationBuilder UseCalculator(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CalculatorMiddleware>();
+        }
+    }
+}
