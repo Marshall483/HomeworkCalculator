7321b9e517cbb95a6c14e9a944feb78b15ed59df
Marshall483 <jows.17@mail.ru>
2020-11-08 21:08:09 +0300

    Now we seed data in DB.

---
+++ MvcMovie/Models/GenerateData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcMovie.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public static class GenerateData
+    {
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Еhe wind is getting stronger",
+                        ReleaseDate = DateTime.Parse("2012-2-12"),
+                        Genre = "Anime",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Shreck",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Animation Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Walking castle",
+                        ReleaseDate = DateTime.Parse("2001-2-23"),
+                        Genre = "Anime",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Harry Potter and Globet of fire",
+                        ReleaseDate = DateTime.Parse("2001-4-15"),
+                        Genre = "Action",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
