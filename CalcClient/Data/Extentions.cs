3a5243c0dc6008d91c4f49c035c8bad98059fe9f
Marshall483 <jows.17@mail.ru>
2020-11-24 18:26:28 +0300

    Added folder data with extentions

---
+++ CalcClient/Data/Extentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingApp.Data
+{
+    public static class CharExtention
+    {
+        public static int Priority(this char ch)
+        {
+            switch (ch)
+            {
+                case '(': case ')': return 1;
+                case '+': case '-': return 2;
+                case '*': case '/': return 3;
+                default: throw new KeyNotFoundException();
+            }
+        }
+    }
+}
