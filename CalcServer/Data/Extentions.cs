83a1b60dd18a4629f42e26070acf97e3099b156d
Marshall483 <jows.17@mail.ru>
2020-11-24 18:32:30 +0300

    Refactored

---
+++ CalcServer/Data/Extentions.cs
@@ -0,0 +1,13 @@
+﻿using CalcOnline.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace CalcOnline.Data.Extentions
+{
+    public static class CalculatorExtention
+    {
+        public static IApplicationBuilder UseCalculator(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CalculatorMiddleware>();
+        }
+    }
+}
