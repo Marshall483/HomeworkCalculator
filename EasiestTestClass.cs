42fa9dffb1229b00c291d7afe1dfb11d530fcc2d
Marshall483 <jows.17@mail.ru>
2020-12-14 18:43:31 +0300

    created tests

---
+++ EasiestTestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace TestBenchmark
+{
+    static class EasiestTestClass
+    {
+        public static void OnTestsStart()
+        {
+            BenchmarkRunner.Run<MyClass>();
+        }
+
+    }
+}
---
+++ MyClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace TestBenchmark
+{
+    public class MyClass
+    {
+        void _simulateWork()
+        {
+            string a = "";
+            for (int i = 0; i < 10000; i++)
+                a += i.ToString();
+        }
+
+       // [Benchmark(Description = "PbStaticVoid")]
+        public static void PbStaticVoid() {
+            string a = "";
+            for (int i = 0; i < 10000; i++)
+                a += i.ToString();
+        }
+
+        [Benchmark(Description = "PbVoid")]
+        public void PbVoid() =>
+            _simulateWork();
+
+        [Benchmark(Description = "PbVirtualVoid")]
+        public virtual void PbVirtualVoid() =>
+            _simulateWork();
+
+        public static void PbGenericStaticVoid<T>()
+        {
+            string a = "";
+            for (int i = 0; i < 10000; i++)
+                a += i.ToString();
+        }
+
+        //[Benchmark(Description = "PbGenericStaticVoid")]
+        public static void PbGenericStaticVoid() =>
+            PbGenericStaticVoid<int>();
+
+
+        [Benchmark(Description = "PbGenericVoid")]
+        public void PbGenericVoid() =>
+            _simulateWork();
+
+        [Benchmark(Description = "PbDynamic")]
+        public dynamic PbDynamic()
+        {
+            string a = "";
+            for (int i = 0; i < 10000; i++)
+                a += i.ToString();
+            return a;
+        }
+
+    }
+}
